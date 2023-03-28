85e78dcc868007994fe743db9dd2178e746d4396
beke83 <bekebenjamin@yahoo.com>
2023-03-28 19:19:25 +0100

    Room auth done

---
+++ api/controllers/roomController.js
@@ -0,0 +1,82 @@
+import Room from "../models/Room.js";
+import Hotel from "../models/Hotel.js";
+// import { createError } from "../utils/error.js";
+
+export const createRoom = async (req, res, next) => {
+
+    const hotelId = req.params.hotelid;
+    const newRoom = new Room(req.body)
+
+    try {
+        const savedRoom = await newRoom.save();
+        console.log(savedRoom);
+        try {
+
+            await Hotel.findByIdAndUpdate(hotelId, { $push: { rooms: savedRoom._id } })
+        }
+        catch (err) {
+            next(err)
+        }
+        res.status(200).json(savedRoom)
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const updateRoom = async (req, res, next) => {
+    try {
+        const updatedRoom = await Room.findByIdAndUpdate(
+            req.params.id,
+            { $set: req.body },
+            { new: true }
+        )
+        res.status(200).json(updatedRoom)
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const deleteRoom = async (req, res, next) => {
+    const hotelId = req.params.hotelid;
+    try {
+        //find the Hotel and update it using the set method
+        await Room.findByIdAndDelete(req.params.id)
+        try {
+            await Hotel.findByIdAndUpdate(hotelId, { $pull: { rooms: req.params.id } })
+        }
+        catch (err) {
+            console.log(error)
+            next(err)
+        }
+        res.status(200).json("Room has been deleted")
+    }
+    catch (err) {
+        console.log(eror)
+        return next(err);
+    }
+}
+
+export const getRoom = async (req, res, next) => {
+    try {
+        //get a hotel by id
+        const room = await Room.findById(req.params.id)
+        res.status(200).json(room)
+    }
+    catch (err) {
+        return next(err);
+    }
+}
+
+export const getRooms = async (req, res, next) => {
+    //const failed = true
+    //if (failed) return next(createError(401, "You are not authenticated!"));
+
+    try {
+        //get all hotel
+        const rooms = await Room.find()
+        res.status(200).json(rooms)
+    }
+    catch (err) {
+        return next(err);
+    }
+}
--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,6 +1,17 @@
 import express from "express"
+import { createRoom, deleteRoom, getRoom, getRooms, updateRoom } from "../controllers/roomController.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
-const router =  express.Router();
+const router = express.Router();
 
+router.post('/:hotelid', verifyAdmin, createRoom);
+
+router.put('/:id', verifyAdmin, updateRoom);
+
+router.delete('/:id/:hotelid', verifyAdmin, deleteRoom);
+
+router.get('/:id', getRoom);
+
+router.get('/', getRooms)
 
 export default router;

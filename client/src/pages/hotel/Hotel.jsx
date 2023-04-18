83745ef2d635a6c92e11d204843f8668228c7622
beke83 <bekebenjamin@yahoo.com>
2023-04-18 14:03:18 +0100

    Implementing function to calc number of days between days

--- client/src/pages/hotel/Hotel.jsx
+++ client/src/pages/hotel/Hotel.jsx
@@ -21,10 +21,18 @@ const Hotel = () => {
 
   const { data, error, loading, reFetch } = useFetch(`/hotels/find/${id}`);
 
-  const { dates } = useContext(SearchContext);
-  // console.log(dates);
+  const { dates, options} = useContext(SearchContext);
+  console.log(dates);
 
   //function to caluate the number of days between a range of dates
+  const MILLISECONDS_PER_DAY = 1000 * 60 * 60 * 24;
+  function dayDifference(date1, date2) {
+    const timeDiff = Math.abs(date2.getTime() - date1.getTime());
+    const diffDays = Math.ceil(timeDiff / MILLISECONDS_PER_DAY);
+    return diffDays
+  }
+
+  const days = dayDifference(dates[0].endDate, dates[0].startDate)
 
   const handleOpen = (i) => {
     setSlideNumber(i);
@@ -49,63 +57,63 @@ const Hotel = () => {
       <Navbar />
       <Header type="list" />
       {
-        loading ? "Loading..." 
-        : (
-          <div className="hotelContainer">
-            {open && (
-              <div className="slider">
-                <FontAwesomeIcon icon={faCircleXmark} className="close" onClick={() => setOpen(false)} />
-                <FontAwesomeIcon icon={faCircleArrowLeft} className="arrow" onClick={() => handleMove("l")} />
-                <div className="sliderWrapper">
-                  <img src={data.photos[slideNumber]} alt="" className="sliderImg" />
-                </div>
-                <FontAwesomeIcon icon={faCircleArrowRight} className="arrow" onClick={() => handleMove("r")} />
-              </div>
-            )}
-            <div className="hotelWrapper">
-              <button className='bookNow'>Reserve or Book Now!</button>
-              <h1 className="hotelTitle">{data.name}</h1>
-              <div className="hotelAddress">
-                <FontAwesomeIcon icon={faLocation} />
-                <span>{data.address}</span>
-              </div>
-              <span className="hotelDistance">
-                Excellent location - {data.distance} from center
-              </span>
-              <span className="hotelPriceHighlight">
-                Book a stay over {data.cheapestPrice} at this property and get a fee airport taxi
-              </span>
-              <div className="hotelImages">
-                {data.photos?.map((photo, i) => (
-                  <div className="hotelImgWrapper">
-                    <img onClick={() => handleOpen(i)} src={photo} alt="hotel_img" className='hotelImg' />
+        loading ? "Loading..."
+          : (
+            <div className="hotelContainer">
+              {open && (
+                <div className="slider">
+                  <FontAwesomeIcon icon={faCircleXmark} className="close" onClick={() => setOpen(false)} />
+                  <FontAwesomeIcon icon={faCircleArrowLeft} className="arrow" onClick={() => handleMove("l")} />
+                  <div className="sliderWrapper">
+                    <img src={data.photos[slideNumber]} alt="" className="sliderImg" />
                   </div>
-                ))}
-              </div>
-              <div className="hotelDetails">
-                <div className="hotelDetailsText">
-                  <h1 className="hotelTitle">{data.title}</h1>
-                  <p className="hotelDesc">
-                    {data.desc}
-                  </p>
+                  <FontAwesomeIcon icon={faCircleArrowRight} className="arrow" onClick={() => handleMove("r")} />
                 </div>
-                <div className="hotelDetailsPrice">
-                  <h1>Perfect for a 9-night stay!</h1>
-                  <span>
-                    Located in the real heart of Krakow, this property has an
-                    excellent location score of 9.8!
-                  </span>
-                  <h2>
-                    <b>$945</b> (9 nights)
-                  </h2>
-                  <button>Reserve or Book Now!</button>
+              )}
+              <div className="hotelWrapper">
+                <button className='bookNow'>Reserve or Book Now!</button>
+                <h1 className="hotelTitle">{data.name}</h1>
+                <div className="hotelAddress">
+                  <FontAwesomeIcon icon={faLocation} />
+                  <span>{data.address}</span>
+                </div>
+                <span className="hotelDistance">
+                  Excellent location - {data.distance} from center
+                </span>
+                <span className="hotelPriceHighlight">
+                  Book a stay over {data.cheapestPrice} at this property and get a fee airport taxi
+                </span>
+                <div className="hotelImages">
+                  {data.photos?.map((photo, i) => (
+                    <div className="hotelImgWrapper">
+                      <img onClick={() => handleOpen(i)} src={photo} alt="hotel_img" className='hotelImg' />
+                    </div>
+                  ))}
+                </div>
+                <div className="hotelDetails">
+                  <div className="hotelDetailsText">
+                    <h1 className="hotelTitle">{data.title}</h1>
+                    <p className="hotelDesc">
+                      {data.desc}
+                    </p>
+                  </div>
+                  <div className="hotelDetailsPrice">
+                    <h1>Perfect for a {days}-night stay!</h1>
+                    <span>
+                      Located in the real heart of Krakow, this property has an
+                      excellent location score of 9.8!
+                    </span>
+                    <h2>
+                      <b>${days * data.cheapestPrice * options.room }</b> ({days} nights)
+                    </h2>
+                    <button>Reserve or Book Now!</button>
+                  </div>
                 </div>
               </div>
+              <Mail />
+              <Footer />
             </div>
-            <Mail />
-            <Footer />
-          </div>
-        )}
+          )}
     </div>
   )
 }
